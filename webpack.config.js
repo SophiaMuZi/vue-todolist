510bda9a94aea0b53aee05c7e6512fe9a790c48b
SophiaMuZi <mu.zi.shan823@gmail.com>
2018-01-22 19:17:47 +0800

    server-dev --Mu

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const htmlPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+    target: 'web',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name]-[hash].[ext]'
+                    }
+                }]
+            },
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new htmlPlugin(),
+    ]
+}
+
+if (isDev) {
+    config.devtool = '#cheap-module-eval-source-map';
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true,
+        },
+        hot: true,
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config;
